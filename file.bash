cbc88993ac7a3b41f655157dd1aee8112afa8488
paperbenni <paperbenni@gmail.com>
2021-09-15 13:09:58 +0200

    fix formatting

--- file.bash
+++ file.bash
@@ -3,59 +3,55 @@
 VERSION="0.0.0"
 
 addfile() {
-	if [ -z "$2" ]
-	then
-		echo "Usage: $PROGRAM file insert file-name filepath"
-		return 1
-	fi
-	
-	local path="$1"
-	local targetfile="$(realpath "$2")"
+    if [ -z "$2" ]; then
+        echo "Usage: $PROGRAM file insert file-name filepath"
+        return 1
+    fi
 
-	if ! [ -e "$targetfile" ]
-	then
-		echo "target $targetfile does not exist"
-	fi
+    local path="$1"
+    local targetfile="$(realpath "$2")"
 
-	local passfile="$PREFIX/$path.file.gpg"
+    if ! [ -e "$targetfile" ]; then
+        echo "target $targetfile does not exist"
+    fi
 
-	check_sneaky_paths "$path"
+    local passfile="$PREFIX/$path.file.gpg"
 
-	if [ -e "$passfile" ]
-	then
-		yesno "file $path already exists, overwrite?"
-	fi
-	# TODO: check for large file size
+    check_sneaky_paths "$path"
 
-	mkdir -p -v "$PREFIX/$(dirname "$path")"
-	set_gpg_recipients "$(dirname "$path")"
-	set_git "$passfile"
+    if [ -e "$passfile" ]; then
+        yesno "file $path already exists, overwrite?"
+    fi
+    # TODO: check for large file size
 
-	$GPG -o "$passfile" --encrypt "${GPG_RECIPIENT_ARGS[@]}" "${GPG_OPTS[@]}" "$targetfile"
+    mkdir -p -v "$PREFIX/$(dirname "$path")"
+    set_gpg_recipients "$(dirname "$path")"
+    set_git "$passfile"
 
-	git_add_file "$passfile" "added encrypted file $path"
+    $GPG -o "$passfile" --encrypt "${GPG_RECIPIENT_ARGS[@]}" "${GPG_OPTS[@]}" "$targetfile"
+
+    git_add_file "$passfile" "added encrypted file $path"
 
 }
 
 exportfile() {
-	local path="$1"
-	local target="$2"
-	local passfile="$PREFIX/$path.file.gpg"
-	if [ -z "$2" ]
-	then
-		echo "Usage: $PROGRAM file export file-name path"
-		return 1
-	fi
-	[ -e "$passfile" ] || {
-		echo "file $path does not exist"
-		return 1
-	}
-	$GPG -o "$target" --decrypt "${GPG_OPTS[@]}" "$passfile" 
-	
+    local path="$1"
+    local target="$2"
+    local passfile="$PREFIX/$path.file.gpg"
+    if [ -z "$2" ]; then
+        echo "Usage: $PROGRAM file export file-name path"
+        return 1
+    fi
+    [ -e "$passfile" ] || {
+        echo "file $path does not exist"
+        return 1
+    }
+    $GPG -o "$target" --decrypt "${GPG_OPTS[@]}" "$passfile"
+
 }
 
 echohelp() {
-	cat << EOF
+    cat <<EOF
 Usage:
 	$PROGRAM file insert file-name filepath
 	    add an encrypted binary file to the password store
@@ -64,20 +60,18 @@ Usage:
 	    to disk as an unencrypted file
 EOF
 
-
 }
 
 case "$1" in
-	insert)
-		shift 1
-		addfile "$1" "$2"
-	;;
-	export)
-		shift 1
-		exportfile "$1" "$2"
-	;;
-	*)
-		echohelp
-	;;
+insert)
+    shift 1
+    addfile "$1" "$2"
+    ;;
+export)
+    shift 1
+    exportfile "$1" "$2"
+    ;;
+*)
+    echohelp
+    ;;
 esac
-
